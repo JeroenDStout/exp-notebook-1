b482fe54c13ebe5b9c34640d643d2ea2556e7dab
JeroenDStout <jeroenstout@gmail.com>
2024-08-18 01:53:17 +0200

    Add proper welcome message

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,8 +1,15 @@
 #include "server/server.h"
 
 #include "server/uwebsockets.h"
+#include "version/git_version.h"
 
 int main() {
+	std::cout
+	  << "Notebook Experiment 1" << gaos::version::get_git_essential_version() << std::endl << std::endl
+	  << gaos::version::get_compile_stamp() << std::endl << std::endl
+	  << gaos::version::get_git_history() << std::endl << std::endl
+    ;
+
     /* Overly simple hello world app */
 	uWS::SSLApp({
 	  .key_file_name = "misc/key.pem",
